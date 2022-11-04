38cc38c46b49bef91a52f6476c46906bb2772308
moritalous <moritalous@gmail.com>
2022-11-04 07:03:19 +0000

    Modify docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://moritalous.github.io/my-docusaurus-v2/',
+  url: 'https://moritalous.github.io/',
   baseUrl: '/my-docusaurus-v2/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
